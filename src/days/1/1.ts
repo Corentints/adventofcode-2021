ffbae93c769f4f7dd9cb1aa3e3a5a74fdd976f1a
Corentin <corentin.tho@gmail.com>
2021-12-01 21:54:18 +0100

    feat: add types

--- src/days/1/1.ts
+++ src/days/1/1.ts
@@ -6,7 +6,7 @@ const entry: Array<string> = entryToArray({
   separator: "\n",
 });
 
-const sumIncreased = (sum, current, iterator, array) => current > array[iterator - 1] ? sum + 1 : sum;
+const sumIncreased = (sum: number, current: number, iterator: number, array: Array<number>): number => current > array[iterator - 1] ? sum + 1 : sum;
 
-const totalIncreased = entry.map((line) => parseInt(line)).reduce(sumIncreased, 0)
+const totalIncreased: number = entry.map((line) => parseInt(line)).reduce(sumIncreased, 0)
 console.log(totalIncreased + " measurements are larger than the previous measurement.");
