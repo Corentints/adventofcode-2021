e55f54b4bd18d5cdb1c90d0f18d8aa817725405a
Corentin <corentin.tho@gmail.com>
2021-12-04 05:03:35 +0100

    feat(day3-e1): add solution

---
+++ src/days/3/1.ts
@@ -0,0 +1,34 @@
+import entryToArray from "../../utils/entryToArray";
+
+const entry: Array<string> = entryToArray({
+  day: 3,
+  entry: 1,
+  separator: "\n",
+});
+
+type Column = {
+  zero: number;
+  one: number;
+};
+
+let gamma = "";
+let epsilon = "";
+
+const reducer = (columns: Array<Column>, current: string): Array<Column> => {
+  current
+    .replace("\r", "")
+    .split("")
+    .forEach((binary, index) => {
+      if (!columns[index]) columns[index] = { zero: 0, one: 0 };
+      Number(binary) ? columns[index].one++ : columns[index].zero++;
+    });
+  return columns;
+};
+
+entry.reduce(reducer, []).forEach((column) => {
+  const sortedValues = column.one > column.zero ? ["1", "0"] : ["0", "1"];
+  gamma += sortedValues[0];
+  epsilon += sortedValues[1];
+});
+
+console.log(gamma, epsilon, parseInt(gamma, 2) * parseInt(epsilon, 2));
