53591ed7818f0c97103eb361061f7347fcc9b787
Corentin <corentin.tho@gmail.com>
2021-12-04 07:20:10 +0100

    feat(day3-e2): improve code

--- src/days/3/2.ts
+++ src/days/3/2.ts
@@ -16,29 +16,21 @@ const getLinesMatchColumnCriteria = (
   colNumber: number,
   mostCommon: boolean = true
 ): string => {
-  if (lines.length === 1) {
-    return lines[0];
-  }
+  if (lines.length === 1) return lines[0];
 
   const occurencesByColumn = lines.reduce(
-    (columns: Array<Column>, current: string): Array<Column> => {
-      if (!columns[colNumber]) columns[colNumber] = { zero: 0, one: 0 };
-      const colValue = current.split("")[colNumber];
-      Number(colValue) ? columns[colNumber].one++ : columns[colNumber].zero++;
-      return columns;
+    (column: Column, current: string): Column => {
+      current.split("")[colNumber] === "1" ? column.one++ : column.zero++;
+      return column;
     },
-    []
+    { zero: 0, one: 0 }
   );
 
-  const sortedValues =
-    occurencesByColumn[colNumber].one >= occurencesByColumn[colNumber].zero
-      ? ["1", "0"]
-      : ["0", "1"];
+  const bitsByOccurence =
+    occurencesByColumn.one >= occurencesByColumn.zero ? ["1", "0"] : ["0", "1"];
 
-  const neededValue = mostCommon ? sortedValues[0] : sortedValues[1];
-  const remainingLines = lines.filter(
-    (line) => line[colNumber] === neededValue
-  );
+  const bitToKeep = mostCommon ? bitsByOccurence[0] : bitsByOccurence[1];
+  const remainingLines = lines.filter((line) => line[colNumber] === bitToKeep);
   return getLinesMatchColumnCriteria(remainingLines, colNumber + 1, mostCommon);
 };
 
